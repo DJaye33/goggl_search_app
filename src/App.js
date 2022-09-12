46e88fd44c4ad0068ea15cf5b4398f938b3a28ec
Danny JeanLouis <dlouis3386@gmail.com>
2022-09-12 12:21:53 -0400

    add components, edit app background

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
+import Routes from "./components/Routes.jsx";
 
 const App = () => {
-  return <div>App</div>;
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  return (
+    <div className={darkTheme ? "dark" : ""}>
+      <div className="bg-gray-500 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+        <Navbar />
+        <Routes />
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default App;
