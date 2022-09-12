c614068f5abc25e75fe34fec118450d20d336827
Danny JeanLouis <dlouis3386@gmail.com>
2022-09-12 13:23:11 -0400

    add styling to App Component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className="p-5 pb-0 flex flex-wrap sm:justify-between">
+      <div className="bg-gray-500 dark:bg-gray-900 dark:text-gray-200 min-h-screen sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200"></div>
     </div>
   );
 };
